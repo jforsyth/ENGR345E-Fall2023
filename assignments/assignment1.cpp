ffc28a4bc190568cba120f1d2cd60e42e605afa7
Jason Forsyth <forsy2jb@jmu.edu>
2023-11-16 11:39:22 -0500

    Remove year

--- assignments/assignment1.cpp
+++ assignments/assignment1.cpp
@@ -6,7 +6,7 @@ int main()
     /**
      *
         For investments over $1M it can be typically assumed that they will return 5% forever.
-        Using the [2022 - 2023 JMU Cost of Attendance](https://www.jmu.edu/financialaid/learn/cost-of-attendance-undergrad.shtml),
+        Using the [JMU Cost of Attendance](https://www.jmu.edu/financialaid/learn/cost-of-attendance-undergrad.shtml),
         calculate how much a rich alumnus would have to give to pay for one full year (all costs) for an in-state student
         and an out-of-state student. 
 
